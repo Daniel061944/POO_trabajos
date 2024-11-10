fb91d930b390889339f81f064be4c417e945d821
Daniel061944 <dagiraldoto@unal.edu.co>
2024-11-10 16:30:19 -0500

    Update ejercicioN4.py

--- Actividad1/ejercicioN4.py
+++ Actividad1/ejercicioN4.py
@@ -1,15 +1,11 @@
-# con el imput se ingresa la edad para la variable fe entrada
-edad_juan = int(input("Ingresa la edad de Juan: "))
-# Calculamos las edades de los demás
-edad_alberto = (2 / 3) * edad_juan
-edad_ana = (4 / 3) * edad_juan
-edad_mama = edad_juan + edad_alberto + edad_ana
-#redondear
-edad_alberto=round(edad_alberto)
-edad_ana =round(edad_ana)
-edad_mama=round(edad_mama)
-# Salida
-print(f"La edad de Juan es {edad_juan} años")
-print(f"La edad de Alberto es {edad_alberto:.0f} años")
-print(f"La edad de Ana es {edad_ana:.0f} años")
-print(f"La edad de la mamá es {edad_mama:.0f} años")
+edadjuan = int(input("Ingresa la edad de Juan: "))
+edadalberto = (2 / 3) * edadjuan
+edadana = (4 / 3) * edadjuan
+edadmama = edadjuan + edadalberto + edadana
+edadalberto=round(edadalberto)
+edadana =round(edadana)
+edadmama=round(edadmama)
+print(f"La edad de Juan es {edadjuan} años")
+print(f"La edad de Alberto es {edadalberto:.0f} años")
+print(f"La edad de Ana es {edadana:.0f} años")
+print(f"La edad de la mamá es {edadmama:.0f} años")
